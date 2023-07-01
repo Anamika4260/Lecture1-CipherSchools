dc1967693fb92eedb218313cf9c67c18b916b293
Anamika4260 <104004726+Anamika4260@users.noreply.github.com>
2023-07-01 22:01:23 +0530

    Add files via upload

---
+++ lec1.cpp
@@ -0,0 +1,11 @@
+what i learned from lecture-1:
+* what is the platform to code on : Vscode
+* How to install vscode.
+* How ro download extensions of cpp in vscode.
+* Whats the environment of coding looks like.
+......................................
+what do i actually need to be serious about while doing this course ?
+* patience to code and follow through the couse 
+* An IDE where you can code patiently .
+* Googling skills
+* Get good in c++ so that you can finally understand those memes 
