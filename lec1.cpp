f75735659cbdb99fb16e4cb5e9e8117b958a7806
Anamika4260 <104004726+Anamika4260@users.noreply.github.com>
2023-07-08 01:22:55 +0530

    Update lec1.cpp

--- lec1.cpp
+++ lec1.cpp
@@ -9,3 +9,8 @@ what do i actually need to be serious about while doing this course ?
 * An IDE where you can code patiently .
 * Googling skills
 * Get good in c++ so that you can finally understand those memes 
+C++ is a powerful and versatile programming language widely used in the software development industry. This course is designed to help you grasp the essential elements of C++ programming, from the basics to more advanced topics. Whether you are a beginner or have some programming experience, this course will provide you with a solid foundation in C++.
+
+In addition to learning C++, we will also delve into the world of data structures and algorithms. Data structures are crucial for organizing and storing data efficiently, while algorithms provide the tools and techniques to process and manipulate that data effectively. By understanding these fundamental concepts, you will be equipped to solve complex problems and optimize your code.
+
+Throughout the course, you will embark on a hands-on learning journey. You will work on various coding exercises, projects, and real-world examples to reinforce your understanding of C++ and data structures algorithms. By the end of the course, you will have the skills and knowledge to build robust programs, implement efficient data structures, and develop optimized algorithms.
